938ad2f91f48486d5bf178744c10190737a69038
CodeIndifferent <84685307+CodeIndifferent@users.noreply.github.com>
2022-07-29 11:41:38 -0400

    Updates

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xd7f056Fc26bF096f32F18ab64201bBBF89b9EaC3";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
